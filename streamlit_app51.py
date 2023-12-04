fa1fcf770755ac1d7001decb0568f7dc27c13c00
wooingyu89 <80017509+wooingyu89@users.noreply.github.com>
2023-12-04 15:11:24 +0900

    Add files via upload

---
+++ streamlit_app51.py
@@ -0,0 +1,34 @@
+import streamlit as st
+
+from langchain_experimental.agents.agent_toolkits import create_csv_agent
+from langchain.agents.agent_types import AgentType
+from langchain import OpenAI
+from dotenv import load_dotenv
+
+def main() :
+
+    st.set_page_config(
+    page_title="WinC Chat",
+    page_icon=":speech_balloon:")
+    
+    st.title(":speech_balloon: Wise InCompany chatbot[:blue[Beta]]")
+
+    filePath="data.csv"
+
+    if filePath is not None:
+        user_quesion = st.text_input("상품 검색어 :")
+
+        llm=OpenAI(temperature=0,
+                   model='gpt-4-0613')
+        agent=create_csv_agent(llm,
+                               filePath,
+                               verbose=True,
+                               agent_type=AgentType.OPENAI_FUNCTIONS,
+                               )
+
+        if user_quesion is not None and user_quesion !="":
+            response = agent.run(user_quesion)
+            st.write(response)
+
+if __name__ == '__main__':
+    main()
