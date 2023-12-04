119f6866d9d1b6da28d08ec05c6615b6a0df3196
wooingyu89 <80017509+wooingyu89@users.noreply.github.com>
2023-12-04 14:36:28 +0900

    Update streamlit_app50.py

--- streamlit_app50.py
+++ streamlit_app50.py
@@ -13,14 +13,14 @@ def main() :
     st.title(":speech_balloon: Wise InCompany chatbot[:blue[Beta]]")
 
     openai_key = st.secrets["openai"]["openai_api_key"]
-    
-    csv_file = st.file_uploader('train.csv', type='csv')
 
-    if csv_file is not None:
+    filePath="data.csv"
+
+    if filePath is not None:
         user_quesion = st.text_input("상품 검색어 :")
 
         llm=OpenAI(temperature=0)
-        agent=create_csv_agent(llm,csv_file, verbose=True)
+        agent=create_csv_agent(llm,filePath, verbose=True)
 
         if user_quesion is not None and user_quesion !="":
             response = agent.run(user_quesion)
