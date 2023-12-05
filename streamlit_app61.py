6099bf9695cd701e1afea9931e380e0fe1dd8241
wooingyu89 <80017509+wooingyu89@users.noreply.github.com>
2023-12-05 13:56:58 +0900

    Update streamlit_app61.py

--- streamlit_app61.py
+++ streamlit_app61.py
@@ -62,9 +62,9 @@ def main() :
         if st.session_state['generated']:
             with response_container:
                 for i in range(len(st.session_state['generated'])):
-                    message(st.session_state['past'][i], is_user=True, key=str(i)+'_user',
+                    message(st.session_state['past'][i], is_user=True, key=str(i)+'_user'
                             )
-                    message(st.session_state['generated'][i], is_user=True, key=str(i),  avatar_style='thumbs')
+                    message(st.session_state['generated'][i], is_user=True, key=str(i))
             
 if __name__ == '__main__':
     main()
