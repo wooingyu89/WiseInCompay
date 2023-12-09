dcc9337c74d98ef54e40c7767177cec09522d0c7
wooingyu89 <80017509+wooingyu89@users.noreply.github.com>
2023-12-09 11:07:38 +0900

    Update streamlit_app65.py

--- streamlit_app65.py
+++ streamlit_app65.py
@@ -133,7 +133,7 @@ def get_vectorstore(text_chunks):
     return vectordb
 
 def get_conversation_chain(vetorestore):
-    llm = ChatOpenAI(model_name = 'gpt-3.5-turbo',temperature=0)
+    llm = ChatOpenAI(openai_api_key, model_name = 'gpt-3.5-turbo',temperature=0)
     conversation_chain = ConversationalRetrievalChain.from_llm(
             llm=llm, 
             chain_type="stuff", 
